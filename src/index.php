06bbab2219d1656fab19a1676e22d941f52c5ad7
Jhonny A. Gomez S. <jhonny@jhoncytech.com>
2024-04-16 18:56:53 +0000

    Actualizacion desde CIRCLECI - 2024-04-16 18:56:53

--- src/index.php
+++ src/index.php
@@ -23,7 +23,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">KEEPCODING 10</a>
+                <a class="nav-link active" aria-current="page" href="#">KEEPCODING 11</a>
             </li>
             </ul>
         </div>
