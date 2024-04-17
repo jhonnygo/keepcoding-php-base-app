c6267e2e3f52e2a89f667a6744baf53c43d904c7
Jhonny A. Gomez S <jhonny@jhoncytech.com>
2024-04-17 13:14:39 +0200

    Modificado el paquete

---
+++ src/tests/SaludoTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Jhoncytech\KcBaseApp\Lib\Saludo;
+
+class SaludoTest extends TestCase
+{
+    public function testObtenerSaludo()
+    {
+        $saludo = new Saludo();
+
+        // Pruebas para distintos rangos de horas
+        $this->assertEquals("Buenas noches", $saludo->obtenerSaludo(0));
+        $this->assertEquals("Buenas noches", $saludo->obtenerSaludo(23));
+        $this->assertEquals("Buenos días", $saludo->obtenerSaludo(6));
+        $this->assertEquals("Buenas tardes", $saludo->obtenerSaludo(15));
+        $this->assertEquals("¡Hola!", $saludo->obtenerSaludo(12));
+    }
+}
