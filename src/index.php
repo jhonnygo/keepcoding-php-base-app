1f555afd73b5b76605bf25abd2bef5b1ba1f287a
Jhonny A. Gomez S. <jhonny@jhoncytech.com>
2024-04-20 17:48:50 +0000

    Actualizacion desde CIRCLECI - 2024-04-20 17:48:50

--- src/index.php
+++ src/index.php
@@ -57,7 +57,7 @@ $horaFormateada = date("H:i", strtotime($horaActual) + (2 * 3600));
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">KEEPCODING 345</a>
+                <a class="nav-link active" aria-current="page" href="#">KEEPCODING</a>
             </li>
             </ul>
         </div>
